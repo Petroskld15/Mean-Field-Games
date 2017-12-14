c5b7f6b99568c52795a1fa56b28a288e45c2b580
marcsv87 <marcsv87@gmail.com>
2017-12-14 10:47:25 +0000

    analytical solution added

--- src/flocking.py
+++ src/flocking.py
@@ -1,6 +1,5 @@
 """
-This script solves the MFG of the flocking model as presented in Probabilistic Theory of Mean Field Games with Applications Chapter 2.4
-I formulate it as an MDP:
+This is all Deterministic
     - X_i_t = speed of player i at time t
     - alpha_i_t = change of speed (or acceleration) of player i at time t. This will be the strategy to optimize
 
@@ -135,11 +134,50 @@ markov_test[0]
 markov_test[1]
 markov_test[2]
 
-# THE BIRDS SPEEDS CONVERGE!
-                
 
-# TODO: compare this with the explicit solution from 
+                
 # 'Probabilistic Theory of Mean Field Games with Applications' eq 2.51
+import math
+import numpy as np
+import random
+
+
+N = 100
+h = 1
+kappa = 1
+sigma = 0.01
+T = 1000
+
+
+def eta(t):
+    val = kappa*(math.sqrt(N/N-1))
+    val = val*math.tanh(kappa*math.sqrt((N - 1)/N)*(T - t))
+    return val
+
+def next_state(x, alpha):
+    next_x = x + alpha*h + sigma*random.gauss(0,1)*math.sqrt(h)
+    return(next_x)
+
+def init_markov():
+    states = np.zeros((N, int(T/h)))
+    states[:,0] = np.random.normal(loc=5,scale=1,size=N)
+    return states
+
+def get_alpha(i,t):
+    val = -1*(1-1/N)*eta(h*t)*(states[i,t]-np.mean(states[:,t]))
+    return val
+
+
+states = init_markov()
+for t in range(1,int(T/h)):
+    for i in range(N):
+        states[i,t] = next_state(states[i,t-1], get_alpha(i,t-1))
+
+states[:,-1]
+
+
+
+
                 
             
 
