bdc7676968af72f3403dbd59e2166b6621336b11
marcsv87 <marcsv87@gmail.com>
2018-01-19 16:58:46 +0000

    Path plots added

--- src/flocking.py
+++ src/flocking.py
@@ -22,7 +22,7 @@ class flocking_model():
 
     def eta(self, t):
         val = self.kappa*(math.sqrt(self.N/(self.N-1)))
-        val = val*math.tanh(self.kappa*math.sqrt((self.N - 1)/self.N)*(self.T - t))
+        val = val*math.tanh(self.kappa*math.sqrt((self.N - 1)/self.N)*(self.T - t*self.h))
         self.etas[int(t)] = val
         return val
     
@@ -34,7 +34,7 @@ class flocking_model():
     
     def init_markov(self):
         states = np.zeros((self.N, int(self.T/self.h),3))
-        states[:,0] = np.random.normal(loc=10, scale=2, size=(self.N,3))
+        states[:,0] = np.random.normal(loc=0, scale=0.3, size=(self.N,3))
         return states
     
 #    def get_alpha(self,i,t):
@@ -50,14 +50,40 @@ class flocking_model():
                 
 
 # example
-flocking_mdp = flocking_model(N=3, h=1, kappa=1, sigma=0.1, T=10)
-flocking_mdp.states[:,0,:]
+flocking_mdp = flocking_model(N=50, h=0.05, kappa=1, sigma=0.2, T=100)
+#flocking_mdp.states[:,0,:]
 x1 = flocking_mdp.next_state(flocking_mdp.states[0,0,:],0)
 flocking_mdp.simulate_mdp()
 flocking_mdp.states[:,-1,:]
-                
-                
 
+
+# We plot the final results velocity vectors (coordinates X and Y) for all the birds
+import matplotlib.pyplot as plt
+
+
+X = np.zeros(flocking_mdp.N)
+Y = np.zeros(flocking_mdp.N)
+U = flocking_mdp.states[:,-1,0]
+V = flocking_mdp.states[:,-1,1]
+C = np.linspace(0,1,flocking_mdp.N)
+
+plt.figure()
+Q = plt.quiver(X,Y,U,V,C, units = 'xy', cmap='jet', width = 0.0005)
+
+# We plot the paths (coordinates X and Y) for all the birds
+X_0 = np.zeros(flocking_mdp.states[:,:,0].shape)
+Y_0 = np.zeros(flocking_mdp.states[:,:,0].shape)
+U = flocking_mdp.states[:,:,0]
+V = flocking_mdp.states[:,:,1]
+X = X_0
+Y = Y_0
+for n in range(0, X.shape[0]):
+    for t in range(1,X.shape[1]):
+        X[n,t] = X[n,t-1]+ flocking_mdp.h*U[n,t-1]
+        Y[n,t] = Y[n,t-1]+ flocking_mdp.h*V[n,t-1]
+    
+plt.figure()
+Q = plt.quiver(X,Y,U,V,C, units = 'xy', cmap='jet',width = 0.005)
 
 
 
