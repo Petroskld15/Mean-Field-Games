916d44d5ab9cc0eb82c9b68b09378e3344cd3f42
marcsv87 <marcsv87@gmail.com>
2018-02-07 17:37:15 +0000

    flocking model updated

--- src/flocking.py
+++ src/flocking.py
@@ -81,9 +81,62 @@ for n in range(0, X.shape[0]):
     for t in range(1,X.shape[1]):
         X[n,t] = X[n,t-1]+ flocking_mdp.h*U[n,t-1]
         Y[n,t] = Y[n,t-1]+ flocking_mdp.h*V[n,t-1]
-    
+C_x = np.linspace(0,1,flocking_mdp.states.shape[1])   
+C_y = np.linspace(0,1,flocking_mdp.states.shape[0])   
+cxx, cyy = np.meshgrid(C_x, C_y)
 plt.figure()
-Q = plt.quiver(X,Y,U,V,C, units = 'xy', cmap='jet',width = 0.005)
+Q = plt.quiver(X,Y,U,V,cyy, units = 'xy', cmap='jet',width = 0.005)
+fig_path = Q.figure
+fig_path.savefig('path.jpg')
+
+
+
+
+
+##### ANIMATIONS
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
+
+# First set up the figure, the axis, and the plot element we want to animate
+X = np.zeros(flocking_mdp.N)
+Y = np.zeros(flocking_mdp.N)
+U = flocking_mdp.states[:,0,0]
+V = flocking_mdp.states[:,0,1]
+C = np.linspace(0,1,flocking_mdp.N)
+
+fig = plt.figure()
+ax = plt.axes(xlim=(-0.05, 0.05), ylim=(-0.05, 0.05))
+vector_field = ax.quiver(X,Y,U,V,C, units = 'xy', cmap='jet',width = 0.0005)
+
+# initialization function: plot the background of each frame
+def init():
+    vector_field.set_UVC(U,V,C)
+    return vector_field,
+
+# animation function.  This is called sequentially
+def animate(i):
+    X = np.zeros(flocking_mdp.N)
+    Y = np.zeros(flocking_mdp.N)
+    U = flocking_mdp.states[:,i,0]
+    V = flocking_mdp.states[:,i,1]
+    vector_field.set_UVC(U,V,C)
+    return vector_field,
+
+# call the animator.  blit=True means only re-draw the parts that have changed.
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=flocking_mdp.states.shape[1], interval=20, blit=True)
+
+# save the animation as an mp4.  This requires ffmpeg or mencoder to be
+# installed.  The extra_args ensure that the x264 codec is used, so that
+# the video can be embedded in html5.  You may need to adjust this for
+# your system: for more information, see
+# http://matplotlib.sourceforge.net/api/animation_api.html
+anim.save('basic_animation.mp4', fps=10, extra_args=['-vcodec', 'libx264'])
+
+plt.show()
+
+
 
 
 
